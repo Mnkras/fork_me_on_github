fba3bd2faf51a244184e4ef668f6208a29fa2ed3
Mnkras <mnkras@gmail.com>
2013-04-01 11:51:15 -0400

    Fixing links

--- blocks/fork_me_on_github/view.php
+++ blocks/fork_me_on_github/view.php
@@ -25,27 +25,27 @@ if($position == 'left') {
 	
 	switch($color) {
 		case "Red":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/5d21241b64dc708fcbb701f68f72f41e9f1fadd6/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png";
 			break;
 			
 		case "Green":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/edc6dae7a1079163caf7f17c60495bbb6d027c93/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png";
 			break;
 
 		case "Black":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/bec6c51521dcc8148146135149fe06a9cc737577/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png";
 			break;
 
 		case "Orange":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/6429057dfef9e98189338d22e7f6646c6694f032/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png";
 			break;	
 
 		case "Grey":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/ce742187c818c67d98af16f96ed21c00160c234a/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png";
 			break;
 
 		case "White":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/c641758e06304bc53ae7f633269018169e7e5851/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png";
 			break;	
 	
 	}
@@ -54,27 +54,27 @@ if($position == 'left') {
 
 	switch($color) {
 		case "Red":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png";
 			break;
 			
 		case "Green":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/abad93f42020b733148435e2cd92ce15c542d320/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png";
 			break;
 
 		case "Black":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png";
 			break;
 
 		case "Orange":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png";
 			break;	
 
 		case "Grey":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png";
 			break;
 
 		case "White":
-			$colorurl = "https://d3nwyuy0nl342s.cloudfront.net/img/4c7dc970b89fd04b81c8e221ba88ff99a06c6b61/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67";
+			$colorurl = "https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png";
 			break;	
 	
 	}
